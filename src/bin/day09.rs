5088db532136b1762743c3f69695dc15b7b00d1b
Jeroen Mandersloot <>
2022-12-09 17:49:41 +0100

    tmp

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -2,22 +2,20 @@ use std::collections::HashSet;
 
 type Pos = (i32, i32);
 
-fn m(head: &Pos, tail: &Pos, direction: &char) -> (Pos, Pos) {
-    let new_head = match direction {
+fn move_head(head: &Pos, direction: &char) -> Pos {
+    match direction {
         'U' => (head.0, head.1 + 1),
         'R' => (head.0 + 1, head.1),
         'D' => (head.0, head.1 - 1),
         'L' => (head.0 - 1, head.1),
         _ => panic!("{}", format!("Invalid direction: {}", direction))
-    };
-
-    let xdiff = (new_head.0 - tail.0).abs();
-    let ydiff = (new_head.1 - tail.1).abs();
-    if (xdiff == 2 && ydiff <= 1) || (xdiff <= 1 && ydiff == 2) {
-        (new_head, *head)
-    } else {
-        (new_head, *tail)
     }
+}
+
+fn move_tail(tail: &Pos, head: &Pos) -> Pos {
+    let xdiff = (head.0 - tail.0).abs();
+    let ydiff = (head.1 - tail.1).abs();
+    return (xdiff == 2 && ydiff <= 2) || (xdiff <= 2 && ydiff == 2);
 }
 
 // fn draw_history(history: &HashSet<Pos>) -> Vec<Vec<char>> {
@@ -46,12 +44,35 @@ fn m(head: &Pos, tail: &Pos, direction: &char) -> (Pos, Pos) {
 //     println!("{}", chart.iter().map(|row| row.iter().collect::<String>()).collect::<Vec<_>>().join("\n"));
 // }
 
-fn part1() -> usize {
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
+// fn part1() -> usize {
+    // let mut head = (0, 0);
+    // let mut tail = (0, 0);
+    // let mut history = HashSet::new();
+    // history.insert(tail);
+    // for line in aoc::io::get_input(9).lines() {
+    //     let mut chars = line.chars();
+    //     let direction = chars.next().unwrap();
+    //     // Skip space
+    //     chars.next();
+    //     // Remaining characters make up the number of times to repeat the move.
+    //     let num = chars.collect::<String>().parse().unwrap();
+    //     for _ in 0..num {
+    //         let (new_head, new_tail) = m(&head, &tail, &direction);
+    //         history.insert(new_tail);
+    //         head = new_head;
+    //         tail = new_tail;
+    //     }
+    // }
+    //
+    // history.len()
+    // 8
+// }
+
+fn solve(num_knots: usize) -> usize {
+    let mut knots = vec![(0, 0); num_knots];
     let mut history = HashSet::new();
-    history.insert(tail);
-    for line in aoc::io::get_input(9).lines() {
+    history.insert((0, 0));
+    for line in aoc::io::get_example(9).lines() {
         let mut chars = line.chars();
         let direction = chars.next().unwrap();
         // Skip space
@@ -59,21 +80,36 @@ fn part1() -> usize {
         // Remaining characters make up the number of times to repeat the move.
         let num = chars.collect::<String>().parse().unwrap();
         for _ in 0..num {
-            let (new_head, new_tail) = m(&head, &tail, &direction);
-            history.insert(new_tail);
-            head = new_head;
-            tail = new_tail;
+            let mut head = knots[0];
+            let mut new_head = move_head(&head, &direction);
+            knots[0] = new_head;
+            for tail in knots.iter_mut().skip(1) {
+                if !move_tail(&tail, &new_head) {
+                    break;
+                }
+                let new_tail = head;
+                head = *tail;
+                new_head = new_tail;
+                *tail = new_tail
+            }
+            println!("{:?}", knots);
+            history.insert(knots[num_knots - 1]);
         }
+        println!("---");
     }
 
     history.len()
 }
 
-fn part2() -> u8 {
-    0
+fn part1() -> usize {
+    solve(2)
+}
+
+fn part2() -> usize {
+    solve(10)
 }
 
 fn main() {
-    println!("{}", part1()); // 6498
+    // println!("{}", part1()); // 6498
     println!("{}", part2());
 }
