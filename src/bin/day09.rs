353fdf0aa3214bb4b05870a7ea1ab192f8397bf7
Jeroen Mandersloot <>
2022-12-09 18:04:35 +0100

    Day 9

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -13,66 +13,42 @@ fn move_head(head: &Pos, direction: &char) -> Pos {
 }
 
 fn move_tail(tail: &Pos, head: &Pos) -> Pos {
-    let xdiff = (head.0 - tail.0).abs();
-    let ydiff = (head.1 - tail.1).abs();
-    return (xdiff == 2 && ydiff <= 2) || (xdiff <= 2 && ydiff == 2);
+    let xdiff = head.0 - tail.0;
+    let ydiff = head.1 - tail.1;
+    let (x, y) = *tail;
+    match (xdiff, ydiff) {
+        // 2 squares away in a single direction.
+        (2, 0) => (x + 1, y),
+        (-2, 0) => (x - 1, y),
+        (0, 2) => (x, y + 1),
+        (0, -2) => (x, y - 1),
+
+        // A knight's move away.
+        (1, 2) => (x + 1, y + 1),
+        (2, 1) => (x + 1, y + 1),
+        (2, -1) => (x + 1, y - 1),
+        (1, -2) => (x + 1, y - 1),
+        (-1, -2) => (x - 1, y - 1),
+        (-2, -1) => (x - 1, y - 1),
+        (-2, 1) => (x - 1, y + 1),
+        (-1, 2) => (x - 1, y + 1),
+
+        // Double diagonal away (only relevant for part 2).
+        (2, 2) => (x + 1, y + 1),
+        (2, -2) => (x + 1, y - 1),
+        (-2, 2) => (x - 1, y + 1),
+        (-2, -2) => (x - 1, y - 1),
+
+        // In all other cases the tail doesn't have to move.
+        _ => (x, y)
+    }
 }
-
-// fn draw_history(history: &HashSet<Pos>) -> Vec<Vec<char>> {
-//     let xs = history.iter().map(|(x, y)| x);
-//     let xmin = xs.min().unwrap();
-//     let xmax = xs.max().unwrap();
-//     let ymin = history.iter().map(|(x, y)| y).min().unwrap();
-//     let ymax = history.iter().map(|(x, y)| y).max().unwrap();
-//     let width = (xmax - xmin + 1) as usize;
-//     let height = (ymax - ymin + 1) as usize;
-//
-//     let row = vec!['.'; width];
-//     let mut chart = Vec::new();
-//     for _ in 0..height {
-//         chart.push(row.clone());
-//     }
-//
-//     for (x, y) in history {
-//         let point = ((x - xmin) as usize, (y - ymin) as usize);
-//         chart[height - 1 - point.1][point.0] = '#';
-//     }
-//
-//     let origin = (-xmin as usize, -ymin as usize);
-//     chart[height - 1 - origin.1][origin.0] = 's';
-//
-//     println!("{}", chart.iter().map(|row| row.iter().collect::<String>()).collect::<Vec<_>>().join("\n"));
-// }
-
-// fn part1() -> usize {
-    // let mut head = (0, 0);
-    // let mut tail = (0, 0);
-    // let mut history = HashSet::new();
-    // history.insert(tail);
-    // for line in aoc::io::get_input(9).lines() {
-    //     let mut chars = line.chars();
-    //     let direction = chars.next().unwrap();
-    //     // Skip space
-    //     chars.next();
-    //     // Remaining characters make up the number of times to repeat the move.
-    //     let num = chars.collect::<String>().parse().unwrap();
-    //     for _ in 0..num {
-    //         let (new_head, new_tail) = m(&head, &tail, &direction);
-    //         history.insert(new_tail);
-    //         head = new_head;
-    //         tail = new_tail;
-    //     }
-    // }
-    //
-    // history.len()
-    // 8
-// }
 
 fn solve(num_knots: usize) -> usize {
     let mut knots = vec![(0, 0); num_knots];
     let mut history = HashSet::new();
     history.insert((0, 0));
-    for line in aoc::io::get_example(9).lines() {
+    for line in aoc::io::get_input(9).lines() {
         let mut chars = line.chars();
         let direction = chars.next().unwrap();
         // Skip space
@@ -84,18 +60,16 @@ fn solve(num_knots: usize) -> usize {
             let mut new_head = move_head(&head, &direction);
             knots[0] = new_head;
             for tail in knots.iter_mut().skip(1) {
-                if !move_tail(&tail, &new_head) {
+                let new_tail = move_tail(&tail, &new_head);
+                if *tail == new_tail {
                     break;
                 }
-                let new_tail = head;
                 head = *tail;
                 new_head = new_tail;
                 *tail = new_tail
             }
-            println!("{:?}", knots);
             history.insert(knots[num_knots - 1]);
         }
-        println!("---");
     }
 
     history.len()
@@ -110,6 +84,6 @@ fn part2() -> usize {
 }
 
 fn main() {
-    // println!("{}", part1()); // 6498
-    println!("{}", part2());
+    println!("{}", part1()); // 6498
+    println!("{}", part2()); // 2531
 }
