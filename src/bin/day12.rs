0d8a606438105b3868a32e9508cd52474e31d03d
Jeroen Mandersloot <>
2022-12-13 09:53:13 +0100

    Some comments

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -16,7 +16,7 @@ impl Graph {
         self.nodes.iter().enumerate().filter(move |(_, node)| **node == value).map(|(idx, _)| idx)
     }
 
-    fn compute_distances(&self, start: usize) -> Vec<usize> {
+    fn compute_distances(&self, start: usize) -> Vec<usize> {  // Dijkstra
         let num_nodes = self.nodes.len();
         let mut distances = vec![std::usize::MAX; num_nodes];
         let mut visited = HashSet::new();
@@ -31,12 +31,12 @@ impl Graph {
             visited.insert(current);
             let neighbours = self.edges.get(current).unwrap().iter().filter(|idx| !visited.contains(idx));
             for neighbour in neighbours {
-                let tmp = match distances[*current] {
+                let distance = match distances[*current] {
                     std::usize::MAX => std::usize::MAX,
                     d => d + 1
                 };
-                if tmp < distances[*neighbour] {
-                    distances[*neighbour] = tmp;
+                if distance < distances[*neighbour] {
+                    distances[*neighbour] = distance;
                 }
             }
         }
@@ -64,6 +64,8 @@ impl Graph {
                 if y < height - 1 { candidates.push((y + 1) * width + x) }
                 if x > 0 { candidates.push(y * width + x - 1) }
                 if x < width - 1 { candidates.push(y * width + x + 1) }
+
+                // Invert all edges because we perform our searches backwards.
                 let min_level = get_level(*nodes.get(idx).unwrap()) - 1;
                 let neighbours = candidates
                     .into_iter()
@@ -87,6 +89,8 @@ fn get_level(value: char) -> usize {
 fn main() {
     let input = aoc::io::get_input(12);
     let graph = Graph::parse(&input);
+
+    // Invert our ``start`` and ``end`` which is necessary for part 2.
     let start = graph.find('E');
     let end = graph.find('S');
     let distances = graph.compute_distances(start);
