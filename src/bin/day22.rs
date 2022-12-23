6e357b197e4fdc5440264e79011e559d0f87dd13
Jeroen Mandersloot <>
2022-12-23 02:05:35 +0100

    Bit cleaner, but only part 2

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -1,6 +1,4 @@
-use std::collections::{HashMap, HashSet};
-use std::fmt::{Display, Formatter, Pointer};
-use std::ops::{Index, Rem};
+use std::collections::HashMap;
 
 use regex::Regex;
 
@@ -8,31 +6,16 @@ type Pos = (i32, i32);
 type Adjacency = HashMap<(Pos, Facing), (Pos, Facing)>;
 
 #[derive(Copy, Clone, Debug, PartialOrd, PartialEq, Ord, Eq, Hash)]
-enum Facing {
-    RIGHT,
-    DOWN,
-    LEFT,
-    UP,
-}
+enum Facing { RIGHT, DOWN, LEFT, UP }
 
 impl Facing {
-    fn all() -> [Facing; 4] {
-        [Facing::RIGHT, Facing::DOWN, Facing::LEFT, Facing::UP]
-    }
-
-    fn turn_right(&self) -> Self {
+    fn all() -> [Facing; 4] { [Facing::RIGHT, Facing::DOWN, Facing::LEFT, Facing::UP] }
+    fn turn_right(&self) -> Self { self.modulo(1) }
+    fn turn_around(&self) -> Self { self.modulo(2) }
+    fn turn_left(&self) -> Self { self.modulo(3) }
+    fn modulo(&self, modulo: usize) -> Self {
         let all = Facing::all();
-        all[(all.iter().position(|f| f == self).unwrap() + 1) % 4]
-    }
-
-    fn turn_left(&self) -> Self {
-        let all = Facing::all();
-        all[(all.iter().position(|f| f == self).unwrap() + 3) % 4]
-    }
-
-    fn turn_around(&self) -> Self {
-        let all = Facing::all();
-        all[(all.iter().position(|f| f == self).unwrap() + 2) % 4]
+        all[(all.iter().position(|f| f == self).unwrap() + modulo) % 4]
     }
 }
 
@@ -78,40 +61,40 @@ impl Cube {
     fn step(&self, pos: Pos, facing: Facing, adj: &Adjacency) -> (Pos, Facing) {
         let (x, y) = pos;
         let dim = self.dim();
-        let sx = x / dim;
-        let sy = y / dim;
+        let side_x = x / dim;
+        let side_y = y / dim;
         let nx = x.rem_euclid(dim);
         let ny = y.rem_euclid(dim);
-        let (candidate, f) = match step_safe((nx, ny), facing, self.dim(), self.dim()) {
+        let (new_pos, new_facing) = match step_safe((nx, ny), facing, self.dim(), self.dim()) {
             Some(_) => (step(pos, facing), facing),
             None => {
-                let ((nsx, nsy), f) = *adj.get(&((sx, sy), facing)).unwrap();
-                let d = dim - 1;
-                let (nnx, nny) = match (facing, f) {
-                    (Facing::RIGHT, Facing::RIGHT) => (0, ny),      
-                    (Facing::RIGHT, Facing::DOWN)  => (d - ny, 0),  
-                    (Facing::RIGHT, Facing::LEFT)  => (d, d - ny),  
-                    (Facing::RIGHT, Facing::UP)    => (ny, d),      // Checked
-                    (Facing::DOWN, Facing::RIGHT)  => (0, d - nx),  
-                    (Facing::DOWN, Facing::DOWN)   => (nx, 0),      
-                    (Facing::DOWN, Facing::LEFT)   => (d, nx),      // Checked
-                    (Facing::DOWN, Facing::UP)     => (d - nx, d),  
-                    (Facing::LEFT, Facing::RIGHT)  => (0, d - ny),  
-                    (Facing::LEFT, Facing::DOWN)   => (ny, 0),      // Checked
-                    (Facing::LEFT, Facing::LEFT)   => (d, ny),      
-                    (Facing::LEFT, Facing::UP)     => (d - ny, d),  
-                    (Facing::UP, Facing::RIGHT)    => (0, nx),      
-                    (Facing::UP, Facing::DOWN)     => (d - nx, 0),  
-                    (Facing::UP, Facing::LEFT)     => (d, nx),      
-                    (Facing::UP, Facing::UP)       => (nx, d),      
+                let d = dim - 1;  // Convenience variable.
+                let ((new_side_x, new_side_y), f) = *adj.get(&((side_x, side_y), facing)).unwrap();
+                let (new_nx, new_ny) = match (facing, f) {
+                    (Facing::RIGHT, Facing::RIGHT) => (0, ny),
+                    (Facing::RIGHT, Facing::DOWN) => (d - ny, 0),
+                    (Facing::RIGHT, Facing::LEFT) => (d, d - ny),
+                    (Facing::RIGHT, Facing::UP) => (ny, d),
+                    (Facing::DOWN, Facing::RIGHT) => (0, d - nx),
+                    (Facing::DOWN, Facing::DOWN) => (nx, 0),
+                    (Facing::DOWN, Facing::LEFT) => (d, nx),
+                    (Facing::DOWN, Facing::UP) => (d - nx, d),
+                    (Facing::LEFT, Facing::RIGHT) => (0, d - ny),
+                    (Facing::LEFT, Facing::DOWN) => (ny, 0),
+                    (Facing::LEFT, Facing::LEFT) => (d, ny),
+                    (Facing::LEFT, Facing::UP) => (d - ny, d),
+                    (Facing::UP, Facing::RIGHT) => (0, nx),
+                    (Facing::UP, Facing::DOWN) => (d - nx, 0),
+                    (Facing::UP, Facing::LEFT) => (d, nx),
+                    (Facing::UP, Facing::UP) => (nx, d),
                 };
 
-                ((nsx * dim + nnx, nsy * dim + nny), f)
+                ((new_side_x * dim + new_nx, new_side_y * dim + new_ny), f)
             }
         };
 
-        if self.get(candidate).unwrap() == '.' {
-            (candidate, f)
+        if self.get(new_pos).unwrap() == '.' {
+            (new_pos, new_facing)
         } else {
             (pos, facing)
         }
@@ -125,35 +108,30 @@ impl Cube {
 
         path.push(pos);
 
-        for (i, &f) in [facing.turn_left(), facing.turn_right()].iter().enumerate() {
-            let a = step(pos, f);
-            if self.sides.contains_key(&a) && !path.contains(&a) {
-                if let Some((b, ff)) = self.get_adjacent_side(a, facing, path) {
+        for rotation in [Facing::turn_left, Facing::turn_right] {
+            let other = step(pos, rotation(&facing));
+            if self.sides.contains_key(&other) && !path.contains(&other) {
+                if let Some((target, facing)) = self.get_adjacent_side(other, facing, path) {
                     path.pop();
-                    return Some((b, if i == 0 { ff.turn_left() } else { ff.turn_right() }));
+                    return Some((target, rotation(&facing)));
                 }
             }
         }
 
-        let opposite = step(pos, facing.turn_around());
-        if self.sides.contains_key(&opposite) && !path.contains(&opposite) {
-            for (i, &facing) in [facing.turn_left(), facing.turn_right(), facing.turn_around()].iter().enumerate() {
-                let a = step(opposite, facing);
-                if self.sides.contains_key(&a) && !path.contains(&a) {
-                    if let Some((b, ff)) = self.get_adjacent_side(a, facing, path) {
+        let other = step(pos, facing.turn_around());
+        if self.sides.contains_key(&other) && !path.contains(&other) {
+            for rotation in [Facing::turn_left, Facing::turn_right, Facing::turn_around] {
+                let opposite = step(other, rotation(&facing));
+                if self.sides.contains_key(&opposite) && !path.contains(&opposite) {
+                    if let Some((target, facing)) = self.get_adjacent_side(opposite, rotation(&facing), path) {
                         path.pop();
-                        let ff = match i {
-                            0 => ff.turn_left(),
-                            1 => ff.turn_right(),
-                            2 => ff.turn_around(),
-                            _ => panic!()
-                        };
-                        return Some((b, ff));
+                        return Some((target, rotation(&facing)));
                     }
                 }
             }
         }
-        return None
+
+        return None;
     }
 
     fn get_adjacency_matrix(&self) -> Adjacency {
@@ -168,113 +146,55 @@ impl Cube {
     }
 }
 
-// impl Display for Cube {
-//     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-//         let res = self.layout.chars().collect::<Vec<_>>().chunks(4).map(|c| c.iter().collect::<String>()).collect::<Vec<_>>().join("\n");
-//         write!(f, "{}", res)
-//     }
-// }
-
-
 fn parse(input: &str, dim: usize) -> (Cube, Vec<&str>) {
     let height = input.lines().count() - 2;
     let width = input.lines().take(height).map(|line| line.len()).max().unwrap();
-
-    let mut a = 0;
     let mut y = 0;
     let mut sides = HashMap::new();
     while y < height {
-        let mut b = 0;
         let mut x = 0;
         while x < width {
             let layout = input.lines().skip(y).take(dim).map(|line| line.chars().skip(x).take(dim).collect::<String>()).collect::<Vec<_>>().join("");
             if layout.trim().len() > 0 {
-                sides.insert((b as i32, a as i32), layout);
+                sides.insert(((x / dim) as i32, (y / dim) as i32), layout);
             }
-            b += 1;
-            x = b * dim;
+            x += dim;
         }
-        a += 1;
-        y = a * dim;
+        y += dim;
     }
 
     let cube = Cube { sides };
-    let mut adj = cube.get_adjacency_matrix();
-    for pos in cube.sides.keys() {
-        for facing in Facing::all() {
-            println!("{:?} --{:?}-> {:?}", pos, facing, adj.get(&(*pos, facing)).unwrap());
-        }
-    }
-
-    // println!("{:?} (should be LEFT)", adj.get(&((3, 2), Facing::RIGHT)).unwrap());
-    // println!("{:?} (should be LEFT)", adj.get(&((3, 2), Facing::UP)).unwrap());
-    // println!("{:?} (should be UP)", adj.get(&((2, 2), Facing::DOWN)).unwrap());
-    // println!("{:?} (should be DOWN)", adj.get(&((2, 1), Facing::RIGHT)).unwrap());
-
     let instructions = Regex::new(r"(\d+|[RL])").unwrap().captures_iter(input.lines().last().unwrap()).map(|captures| captures.iter().next().unwrap().unwrap().as_str()).collect();
     (cube, instructions)
 }
 
-// fn draw(grid: &Grid) -> String {
-//     let width = grid.keys().map(|(x, _)| *x).max().unwrap();
-//     let height = grid.keys().map(|(_, y)| *y).max().unwrap();
-//     let mut drawing = String::from("");
-//     for y in 0..height {
-//         for x in 0..width {
-//             drawing.push(match grid.get(&(x, y)).unwrap() {
-//                 None => ' ',
-//                 Some(true) => '#',
-//                 Some(false) => '.',
-//             });
-//         }
-//         drawing.push('\n');
-//     }
-//     drawing
-// }
-
-fn main() {
-    let input = aoc::io::get_input(22);
-    let (cube, instructions) = parse(&input, 50);
-
-
+fn part2(cube: &Cube, instructions: &Vec<&str>) -> i32 {
     let adj = cube.get_adjacency_matrix();
-    let mut curr_pos = (cube.dim() * *cube.sides.keys().filter(|(x, y)| *y == 0).map(|(x, y)| x).min().unwrap(), 0);
-    let mut curr_facing = Facing::RIGHT;
-
-    println!("{:?}", curr_pos);
+    let mut pos = (cube.dim() * cube.sides.keys().filter(|(_, y)| *y == 0).map(|(x, _)| *x).min().unwrap(), 0);
+    let mut facing = Facing::RIGHT;
     for instruction in instructions {
         match instruction.parse::<i32>() {
             Ok(num_steps) => {
                 for _ in 0..num_steps {
-                    let (a, b) = cube.step(curr_pos, curr_facing, &adj);
-                    if curr_pos == a {
-                        break;
-                    }
-                    curr_pos = a;
-                    curr_facing = b;
-                    println!("{:?} facing {:?}", curr_pos, curr_facing);
+                    let (new_pos, new_facing) = cube.step(pos, facing, &adj);
+                    if pos == new_pos { break; }
+                    pos = new_pos;
+                    facing = new_facing;
                 }
-            },
+            }
             Err(_) => {
-                curr_facing = match instruction {
-                    "R" => curr_facing.turn_right(),
-                    "L" => curr_facing.turn_left(),
-                    _ => panic!()
-                };
+                facing = if *instruction == "R" { facing.turn_right() } else { facing.turn_left() };
             }
         }
     }
 
-    let solution = 1000 * (curr_pos.1 + 1) + 4 * (curr_pos.0 + 1) + match curr_facing {
-        Facing::RIGHT => 0,
-        Facing::DOWN => 1,
-        Facing::LEFT => 2,
-        Facing::UP => 3,
-    };
+    let facing_idx = Facing::all().iter().position(|&f| f == facing).unwrap() as i32;
+    let (x, y) = pos;
+    1000 * (y + 1) + 4 * (x + 1) + facing_idx
+}
 
-    println!("{solution}");
-
-
-    // println!("{:?}", part1(&input));
-    // println!("{}", part2(&input, is_example));
+fn main() {
+    let input = aoc::io::get_input(22);
+    let (cube, instructions) = parse(&input, 50);
+    println!("{}", part2(&cube, &instructions));
 }
